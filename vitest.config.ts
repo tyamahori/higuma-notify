45783300a227794239b5ccb0925a98d989ea8ae9
k47320 <sample@sample.com>
2025-08-24 16:27:17 +0900

    Cloudflare Workersが不要なので、vitest-environment-miniflareを削除

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'miniflare',
-    environmentOptions: {
-      bindings: {
-        DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/test/test'
-      }
-    },
     globals: true,
     setupFiles: ['./tests/setup.ts']
   }
