5896393b89b5b433328e804acef73a8c7c266f6b
tyamahori <tyamahori@gmail.com>
2025-08-30 02:23:11 +0900

    Discordに渡すデータを型で渡すようにして責務を整理

--- src/parseXml.ts
+++ src/parseXml.ts
@@ -1,13 +1,18 @@
-import { DiscordContent } from './types/youtubeXmlInterface';
+import { DiscordContent, YouTubeFeed, youTubeFeedSchema } from './types/youtubeXmlInterface';
 import { FuncResult } from './types/funcResult';
 import z from 'zod';
-import { XmlParser } from './youtubeFeedParser';
+import { XmlParser } from './XmlParser';
 
 export const parseYouTubeXml = (body: string): FuncResult<DiscordContent, z.ZodError | unknown> => {
   try {
-    const content: DiscordContent = XmlParser(body);
+    const xml: YouTubeFeed = youTubeFeedSchema.parse(XmlParser(body));
     // 検証が成功したため、`xml`はYouTubeFeed型として扱える
-    console.log('XML検証成功:', content.title);
+    console.log('XML検証成功:', xml.feed.title);
+    const content: DiscordContent = {
+      message: '新着動画だよ！（暖かみのあるbot）',
+      title: xml.feed.entry.title,
+      url: xml.feed.entry.link['@_href'],
+    };
     return { success: true, data: content };
   } catch (error) {
     // 検証に失敗した場合、ZodErrorがスローされる
