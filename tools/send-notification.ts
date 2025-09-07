f8a89c48ec41074fc1e7def155684d8c9741d1bf
Yusuke Tanaka <wing0920@gmail.com>
2025-09-07 13:57:06 +0900

    add tools/send-notification.ts

---
+++ tools/send-notification.ts
@@ -0,0 +1,30 @@
+import { createHmac } from "node:crypto";
+
+function stringEnv(name: string): string {
+    const value = process.env[name];
+    if (!value) {
+        throw new Error(`Environment variable ${name} is required`);
+    }
+    return value;
+}
+
+const HUB_CALLBACK = stringEnv("HUB_CALLBACK");
+const HUB_SECRET = stringEnv("HUB_SECRET");
+const PAYLOAD = stringEnv("PAYLOAD");
+
+const hmac = createHmac("sha1", HUB_SECRET).update(PAYLOAD).digest("hex");
+
+const res = await fetch(HUB_CALLBACK, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/atom+xml",
+        "X-Hub-Signature": `sha1=${hmac}`,
+    },
+    body: PAYLOAD,
+});
+
+const resBody = await res.text();
+console.log({
+    status: res.status,
+    resBody,
+});
