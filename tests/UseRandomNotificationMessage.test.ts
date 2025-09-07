a45418c8db81a26c7d44553c39165263f052483f
hi2k1978 <hi2k1978@gmail.com>
2025-09-07 21:44:05 +0900

    feat: add error case test in UseRandomNOtificationMessage.test.ts

--- tests/UseRandomNotificationMessage.test.ts
+++ tests/UseRandomNotificationMessage.test.ts
@@ -39,4 +39,41 @@ describe('Test UseRandomNotificationMessage', () => {
       expect(generateRandomNotificationMessage()).toBe('test message 4');
     });
   });
+
+  describe('Random Notification Message Error Case Test', () => {
+    it('should throw error when notification messages are empty', () => {
+      const { generateRandomNotificationMessage } = useRandomNotificationMessage([], () => 0);
+      expect(() => generateRandomNotificationMessage()).toThrow('通知メッセージなし');
+    });
+
+    it('should throw error when notification messages are more than max length (Boundary Value Test)', () => {
+      const notificationMessagesInBoundary: string[] = Array.from(
+        { length: 256 },
+        (_, i) => `notification message ${i}`
+      );
+      const notificationMessagesOutOfBoundary: string[] = Array.from(
+        { length: 257 },
+        (_, i) => `notification message ${i}`
+      );
+      const { generateRandomNotificationMessage: generateRandomNotificationMessageInBoundary } =
+        useRandomNotificationMessage(notificationMessagesInBoundary, () => 0);
+
+      expect(generateRandomNotificationMessageInBoundary()).toBe('notification message 0');
+
+      const { generateRandomNotificationMessage: generateRandomNotificationMessageOutOfBoundary } =
+        useRandomNotificationMessage(notificationMessagesOutOfBoundary, () => 0);
+
+      expect(() => generateRandomNotificationMessageOutOfBoundary()).toThrow(
+        '通知メッセージの数が大杉'
+      );
+    });
+
+    it('should throw error when random generator returns non-integer', () => {
+      const { generateRandomNotificationMessage } = useRandomNotificationMessage(
+        ['test'],
+        () => 3.14
+      );
+      expect(() => generateRandomNotificationMessage()).toThrow('生成した乱数が非整数');
+    });
+  });
 });
