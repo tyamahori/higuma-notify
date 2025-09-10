9b4e1ad99cda72b640429ff54a91dc16a82ac1eb
hi2k1978 <hi2k1978@gmail.com>
2025-09-10 22:09:36 +0900

    feat: add key-value store implementation to check whether new youtube feed is already notified.

--- src/UseKeyValueStore.ts
+++ src/UseKeyValueStore.ts
@@ -1,23 +1,28 @@
-// TODO: 不要？
 export class KeyValueStoreError extends Error {
   static {
     this.prototype.name = 'KeyValueStoreError';
   }
 }
 
-// TODO: KV STOREを注入する場合は、引数に記載
-export const useKeyValueStore = () => {
+export const useKeyValueStore = (kv: KVNamespace) => {
   const isKeyAlreadyStored = async (key: string): Promise<boolean> => {
-    // TODO: key が登録済みかチェック処理を記載
-    // TODO: try / catch で KV STORE に関するエラー処理も記載
-    console.log(`key: ${key}`);
-    return false;
+    try {
+      return (await kv.get(key)) ? true : false;
+    } catch (error: unknown) {
+      throw new KeyValueStoreError('Key Value Store: Key Registeration Check Error', {
+        cause: error,
+      });
+    }
   };
 
   const storeKeyValue = async (key: string, value: string): Promise<void> => {
-    // TODO: key / value の登録処理を記載
-    // TODO: try / catch で KV STORE に関するエラー処理も記載
-    console.log(`[key, value]: [${key}, ${value}]`);
+    try {
+      await kv.put(key, value);
+    } catch (error: unknown) {
+      throw new KeyValueStoreError('Key Value Store:Key Value Registeration Error', {
+        cause: error,
+      });
+    }
   };
 
   return { isKeyAlreadyStored, storeKeyValue };
